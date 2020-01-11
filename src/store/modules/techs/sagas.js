1e1d57e039b8d8549a7dfadd0865087d24da519d
Josué Gimenes <josuegalvares@gmail.com>
2020-01-11 16:04:03 -0300

    Axios Mock

--- src/store/modules/techs/sagas.js
+++ src/store/modules/techs/sagas.js
@@ -7,6 +7,9 @@ export function* getTechs() {
   try {
     const response = yield call(api.get, 'techs');
 
+    // Visualiza o retorno fake do mock do sagas.
+    // console.log(response.data);
+
     yield put(getTechsSuccess(response.data));
 
   }catch (err) {
